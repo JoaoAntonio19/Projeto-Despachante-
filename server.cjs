6eeead71417b33d01af4406415db96da41f9eaf4
João Antônio Oliveira <joaoantoniooliveira72@gmail.com>
2026-05-17 23:27:19 -0300

    mudando nodemailer para porta 465 com conexao segura

--- server.cjs
+++ server.cjs
@@ -29,14 +29,16 @@ function autenticar(req, res, next) {
   }
 }
 
-// ─── Nodemailer ───────────────────────────────────────────────────────────────
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
+  host: parseInt(process.env.EMAIL_PORT) || 465,
   port: Number(process.env.EMAIL_PORT),
-  secure: false,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false 
   }
 });
 
